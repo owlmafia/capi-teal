8e8ea72e61562572fd781d64231d334d93a64842
Ivan Schuetz <ivanschuetz@gmail.com>
2022-08-15 17:29:23 +0200

    Move set app args to new file

--- pyteal/dao_app.py
+++ pyteal/dao_app.py
@@ -5,6 +5,7 @@ from common.constants import *
 from common.payments import *
 from common.inner_txs import *
 from common.state import *
+from domain.setup_dao import *
 
 """App central approval"""
 
@@ -37,44 +38,7 @@ def approval_program():
         # the asset transferred is what will be stored as shares asset id
         Assert(Gtxn[2].xfer_asset() == Btoi(Gtxn[1].application_args[0])),
 
-        # initialize state
-        set_gs(GLOBAL_RECEIVED_TOTAL, Int(0)),
-        set_gs(GLOBAL_AVAILABLE_AMOUNT, Int(0)),
-        set_gs(GLOBAL_LOCKED_SHARES, Int(0)),
-
-        set_gs(GLOBAL_SHARES_ASSET_ID, Btoi(Gtxn[1].application_args[0])),
-        set_gs(GLOBAL_FUNDS_ASSET_ID, Btoi(Gtxn[1].application_args[1])),
-
-        set_gs(GLOBAL_DAO_NAME, Gtxn[1].application_args[2]),
-        set_gs(GLOBAL_DAO_DESC, Gtxn[1].application_args[3]),
-        set_gs(GLOBAL_SHARE_PRICE, Btoi(Gtxn[1].application_args[4])),
-        set_gs(GLOBAL_INVESTORS_PART, Btoi(Gtxn[1].application_args[5])),
-
-        set_gs(GLOBAL_SOCIAL_MEDIA_URL, Gtxn[1].application_args[6]),
-
-        set_gs(GLOBAL_VERSIONS, Gtxn[1].application_args[7]),
-
-        # for now commented: on one side backwards compatibility with tests,
-        # on the other, maybe we want to make funding target (and date) optional, 
-        # so the project can start immediately and raise funds indefinitely..
-        # (not sure whether this is legally possible)
-        # in any case: min funds target 0 and end date "now", makes it effectively optional 
-        # (as this moves the project effectively in "funds successfully raised" state)
-        # Assert(Btoi(Gtxn[1].application_args[8]) > Int(0)), # sanity check: there should be always a (positive) funding target
-
-        set_gs(GLOBAL_TARGET, Btoi(Gtxn[1].application_args[8])),
-        set_gs(GLOBAL_TARGET_END_DATE, Btoi(Gtxn[1].application_args[9])),
-
-        set_gs(GLOBAL_SETUP_DATE, Btoi(Gtxn[1].application_args[10])),
-
-        set_gs(GLOBAL_PROSPECTUS_URL, Gtxn[1].application_args[11]),
-        set_gs(GLOBAL_PROSPECTUS_HASH, Gtxn[1].application_args[12]),
-
-        set_gs(GLOBAL_RAISED, Int(0)),
-
-        set_gs(GLOBAL_MIN_INVEST_AMOUNT, Btoi(Gtxn[1].application_args[13])),
-        set_gs(GLOBAL_MAX_INVEST_AMOUNT, Btoi(Gtxn[1].application_args[14])),
-
+        dao_setup_init_global_state(Gtxn[1].application_args),
 
         # checks depending on global state
 
